fe593cf553a973ec5cf76844dae83b526a16b26a
Adil Salim <1awesomeadil@gmail.com>
2022-04-13 19:25:15 +0200

    Commit

---
+++ src/components/Slider.jsx
@@ -0,0 +1,90 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+`;
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vw;
+  display: flex;
+  align-items: center;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+
+const Image = styled.img`
+  height: 80%;
+`;
+
+const Title =styled.h1`
+
+`;
+
+const Desc =styled.p`
+    
+`;
+
+const Button =styled.button`
+    
+`;
+
+const Slider = () => {
+  return (
+    <Container>
+      <Arrow direction="left">
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImgContainer>
+            <Image src="https://i.pinimg.com/originals/d8/b1/e4/d8b1e43f302319420788b06c576e9126.jpg" />
+          </ImgContainer>
+          <InfoContainer>
+              <Title>WINTER SALE</Title>
+              <Desc>30% OFF ON NEW ITEMS</Desc>
+              <Button>SHOW NOW</Button>
+          </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
