459277f1713eee8a3897812cfb6d11d2ffd452cc
Adil Salim <1awesomeadil@gmail.com>
2022-04-21 20:35:06 +0200

    Let's hope

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
         <Route exact path="/productList" element={<ProductList/>}/>
         <Route exact path="/register" element={<Register/>}/>
       </Routes>
+      <Home/>
       </BrowserRouter>
       
     </>
