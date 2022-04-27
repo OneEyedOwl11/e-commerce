3ed35d900cbafe4e3f1e38fb65eb3748033b88bd
Adil Salim <1awesomeadil@gmail.com>
2022-04-27 10:19:50 +0200

    Hope this works

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import './App.css';
-import Cart from './pages/Cart';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Product from './pages/Product';
-import ProductList from './pages/ProductList';
-import Register from './pages/Register';
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import "./App.css";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-  <>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/cart" element={<Cart/>}/>
-        <Route exact path="/login" element={<Login/>}/>
-        <Route exact path="/product" element={<Product/>}/>
-        <Route exact path="/productList" element={<ProductList/>}/>
-        <Route exact path="/register" element={<Register/>}/>
-      </Routes>
-      <Home/>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/e-commerce" element={<Home />} />
+          <Route exact path="/cart" element={<Cart />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/product" element={<Product />} />
+          <Route exact path="/productList" element={<ProductList />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
       </BrowserRouter>
-      
     </>
   );
 }
